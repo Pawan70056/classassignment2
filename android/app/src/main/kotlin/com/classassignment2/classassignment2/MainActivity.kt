6983c667d8f67a27434ec59dc26006f4e9dca597
Pawan70056 <ssmp70056@gmail.com>
2024-12-27 12:19:19 +0545

    Class Assignment 2

---
+++ android/app/src/main/kotlin/com/classassignment2/classassignment2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.classassignment2.classassignment2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:classassignment2/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    StudentApp(),
+  );
+}
